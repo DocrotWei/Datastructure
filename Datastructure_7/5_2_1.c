8eea001e47ffb6addfc782dcbdb6ccab12ba77dd
DocrotWei <894042825@qq.com>
2020-03-03 19:04:24 +0800

    5-2.2AC

--- Datastructure_7/5_2_1.c
+++ Datastructure_7/5_2_1.c
@@ -2,25 +2,30 @@
 #include<stdlib.h>
 #include<math.h>
 
+#define MaxSize 10007
 /*-------------散列表定义-----------*/
 typedef int ElementType;
 enum EntryType {Legitimate, Empty};//Empty 为无人占用
-struct HashEntry{
+typedef struct HashNode{
     ElementType Element;//存放元素
-    enum EntryType Info;//Occupation Sign
-};
+    ElementType Info;//Occupation Sign
+}HashEntry[MaxSize] ;
 
 struct HNode{
     int TableSize;
-    struct HashEntry *TheCells;
+    HashEntry TheCells;
 };
 typedef struct HNode *HashTable;
 /*-------------------------------------*/
-
+/*---------------寻找素数部分-----------*/
 int IsPrime(int Num){/*返回1为素数， 返回0为非素数*/
     int i;
+    if(Num == 1 || Num == 0)
+        return 0;
+    if(Num == 2)
+        return 1;
     for(i = 2; i <= sqrt(Num); i++){
-        if((Num % i) == 0)
+        if((Num % (int)i) == 0)
             return 0;
     }
     return 1;
@@ -33,43 +38,32 @@ int FindPrime(int TableSzie){
     }
     return Prime;
 }
+/*------------------------------------*/
 
 HashTable InitializeTable(int TableSize){
     HashTable H = (HashTable)malloc(sizeof(struct HNode));
-    if(IsPrime(TableSize))
-        H->TableSize = TableSize;
-    else
-        H->TableSize = FindPrime(TableSize);
-    H->TheCells = (struct HashEntry*)malloc(sizeof(struct HashEntry) * TableSize);
+    TableSize = H->TableSize = FindPrime(TableSize);
     while(TableSize){
-        H->TheCells[--TableSize].Info = Empty;
+        H->TheCells[--TableSize].Info = 0;
+        H->TheCells[TableSize].Element = 0;
     }
     return H;
 }
-
-
 
 int Hash(ElementType Key, HashTable H){
     return Key % H->TableSize;
 }
 
 int FindPos(ElementType Key, HashTable H){
-    int Next,Pos = Hash(Key, H), cnt = 0, j;
+    int Next,Pos = Hash(Key, H), cnt = 1;
     Next = Pos;
-    while((H->TheCells[Next].Info != Empty) && (Key ==  H->TheCells[Next].Element)){
-        if(++cnt % 2){
-            Next = Pos + (((cnt+1) * (cnt+1))>>2);
-            if(Next >= H->TableSize)
-                Next -= H->TableSize;
-        }
-        else
-        {
-            Next = Pos - ((cnt * cnt)>>2);
-            if(Next < 0)
-                Next += H->TableSize;
-        }
-        j++;
-        if(j == H->TableSize)
+    while(H->TheCells[Next].Info != 0 && H->TheCells[Next].Element != Key){
+        Next = Pos + ((cnt) * (cnt));
+        cnt++;
+        if(Next >= H->TableSize)
+            Next = Next % H->TableSize;
+
+        if(cnt == H->TableSize)
             return -1;
     }
     return Next;
@@ -81,9 +75,9 @@ void InsertAndOutput(ElementType Key, HashTable H){
         printf("-");
         return;
     }
-    if(H->TheCells[pos].Info == Empty){
+    if(H->TheCells[pos].Info == 0){
         H->TheCells[pos].Element = Key;
-        H->TheCells[pos].Info = Legitimate;
+        H->TheCells[pos].Info = 1;
         printf("%d", pos);
     }
 }
@@ -92,7 +86,7 @@ int main(){
     HashTable H;
     int TableSize, Num;
     ElementType Key;
-    scanf("%d %d", &Num, &TableSize);
+    scanf("%d %d", &TableSize, &Num);
     H = InitializeTable(TableSize);
     scanf("%d", &Key);
     InsertAndOutput(Key, H);
